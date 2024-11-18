559d39f58258ee79daaeb265a486e855f322aed2
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-11-19 01:15:10 +0900

    Feat: 실패, 성공 메세지 코드 구현 - #8

---
+++ src/main/java/com/sopt/agoda/common/response/code/SuccessMessage.java
@@ -0,0 +1,33 @@
+package com.sopt.agoda.common.response.code;
+
+import org.springframework.http.HttpStatus;
+
+public enum SuccessMessage {
+    /**
+     * 200 OK
+     */
+    OK(HttpStatus.OK, "s2000", "요청이 성공했습니다."),
+
+    /**
+     * 201 Created
+     */
+    CREATED(HttpStatus.CREATED, "s2010", "요청이 성공했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    private SuccessMessage(HttpStatus httpStatus, String code, String message) {
+        this.httpStatus = httpStatus;
+        this.code = code;
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
