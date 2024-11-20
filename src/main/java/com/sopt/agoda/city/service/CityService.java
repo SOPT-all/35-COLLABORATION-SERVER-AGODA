3488b0b4d34f764387628ab1676129abf26ebe67
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-11-21 01:10:07 +0900

    Feat: CityService 구현 - #13

--- src/main/java/com/sopt/agoda/city/service/CityService.java
+++ src/main/java/com/sopt/agoda/city/service/CityService.java
@@ -1,8 +1,13 @@
 package com.sopt.agoda.city.service;
 
+import com.sopt.agoda.city.controller.dto.res.CitiesRes;
 import com.sopt.agoda.city.controller.dto.res.PopularCitiesRes;
 import com.sopt.agoda.city.domain.City;
 import com.sopt.agoda.city.repository.CityRepository;
+import com.sopt.agoda.common.exception.AgodaException;
+import com.sopt.agoda.common.response.message.FailMessage;
+import com.sopt.agoda.country.domain.Country;
+import com.sopt.agoda.country.repository.CountryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +15,11 @@ import java.util.List;
 @Service
 public class CityService {
     private final CityRepository cityRepository;
+    private final CountryRepository countryRepository;
 
-    public CityService(CityRepository cityRepository) {
+    public CityService(CityRepository cityRepository, CountryRepository countryRepository) {
         this.cityRepository = cityRepository;
+        this.countryRepository = countryRepository;
     }
 
     public PopularCitiesRes getPopularCities() {
@@ -21,5 +28,17 @@ public class CityService {
                 city -> PopularCitiesRes.CitiesInfo.of(city.getId(), city.getName(), city.getHotelCount(), city.getImageUrl())
         ).toList();
         return PopularCitiesRes.of(citiesInfo);
+    }
+
+    public CitiesRes getCities() {
+        final List<City> foundCities = cityRepository.findAll();
+        final List<CitiesRes.SearchCityInfo> searchCityInfos = foundCities.stream().map(
+                city -> {
+                    final Country foundCountry = countryRepository.findById(city.getCountryId()).orElseThrow(
+                            () -> new AgodaException(FailMessage.NOT_FOUND_ENTITY));
+                    return CitiesRes.SearchCityInfo.of(city.getId(), city.getName(), foundCountry.getName());
+                }
+        ).toList();
+        return CitiesRes.of(searchCityInfos);
     }
 }
